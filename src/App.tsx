22209c4d0e66473f2318187a276e10ad052ee95b
KaeTheDev <kaethedev@gmail.com>
2026-01-27 11:42:09 -0500

    Installed react-router-dom, added BrowserRouter to main.tsx file, created a route for Home page, and updated package.json to include GITHUB_PAGES variable to fix issue with site running locally and for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-       <h1 className="text-3xl text-blue-500">KaeTheDev is the GREATEST!</h1>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base:'/Recipe-Box/',
+  base: process.env.GITHUB_PAGES ? '/Recipe-Box/' : '/',
   plugins: [react(), tailwindcss()],
 })
