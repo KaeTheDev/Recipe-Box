c7831836ea4ffb2dd363b08bf008dfb48fece552
KaeTheDev <kaethedev@gmail.com>
2026-01-27 22:39:17 -0500

    Made hero full size again

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import QuickPickSection from "../components/QuickPickSection/QuickPickSection";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col w-full mx-auto max-w-7xl">
+    <main>
       <HeroSection />
 
       <section className="px-4 sm:px-6 lg:px-8 py-10 bg-gray-50 mb-5">
