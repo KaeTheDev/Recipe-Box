928e88f0ee8d2789d38ed38d113054c4b0b0033a
KaeTheDev <kaethedev@gmail.com>
2026-01-28 15:56:28 -0500

    created static instructions section and added it to recipedetailpage for testing

--- src/pages/RecipeDetailPage.tsx
+++ src/pages/RecipeDetailPage.tsx
@@ -1,11 +1,13 @@
 import RecipeDetailHero from "../components/RecipeDetailHero/RecipeDetailHero"
 import RecipeDetails from "../components/RecipeDetails/RecipeDetails"
+import RecipeIngredients from "../components/RecipeIngredients/RecipeIngredients"
 
 export default function RecipeDetailPage() {
     return(
         <>
         <RecipeDetailHero />
         <RecipeDetails />
+        <RecipeIngredients />
         </>
     )
 }
