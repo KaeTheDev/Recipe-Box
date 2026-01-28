7a958d92a3c05d1cfc32183aa358e2f93d68fbe3
KaeTheDev <kaethedev@gmail.com>
2026-01-28 14:43:59 -0500

    Change icons to orange for favoritepage. Created static ShoppingPage and added route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import Favorites from "./pages/FavoritePage";
+import Shopping from "./pages/ShoppingPage";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
+        <Route path="/shopping-list" element={<Shopping />} />
       </Routes>
     </>
   );
--- src/pages/ShoppingPage.tsx
+++ src/pages/ShoppingPage.tsx
@@ -1,26 +1,29 @@
-import { Heart } from "lucide-react";
+import { ShoppingBag } from "lucide-react";
 
-export default function FavoritePage() {
+export default function ShoppingPage() {
   return (
     <>
       {/* Header Section */}
       <section className="bg-orange-50 py-10 px-4">
         <div className="max-w-5xl mx-auto flex flex-col gap-2">
           <div className="flex items-center gap-2">
-            <Heart size={24} className="text-orange-500" />
-            <h2 className="text-3xl font-semibold">Favorites</h2>
+            <ShoppingBag size={24} className="text-orange-500" />
+            <h2 className="text-3xl font-semibold">Shopping List</h2>
           </div>
-          <p className="text-sm text-gray-600">2 Favorite recipes</p>
+          <p className="text-sm text-gray-600">2 Items</p>
         </div>
       </section>
 
       {/* Empty State */}
       <section className="flex justify-center px-4">
         <div className="flex flex-col items-center text-center mt-10 gap-4 px-6 py-8 bg-white rounded-lg shadow-md max-w-md w-full">
-          <Heart size={36} className="text-red-500" />
-          <h3 className="text-2xl font-semibold">No favorites yet</h3>
+          <ShoppingBag size={36} className="text-orange-500" />
+          <h3 className="text-2xl font-semibold">
+            Your Shopping List Is Empty
+          </h3>
           <p className="text-sm text-gray-600">
-            Mark recipes as favorites by tapping the heart icon. Your favorite recipes will appear here for quick access!
+            Add recipes to your shopping list to see ingredients here. Items
+            from multiple recipes are automatically combined!
           </p>
           <button className="bg-orange-500 py-3 px-6 text-white rounded-md hover:bg-orange-600 transition">
             Browse Recipes
