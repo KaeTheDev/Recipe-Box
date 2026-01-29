610f12d1c1fbfd23dbb23c1f49c23dc6c92c1fd3
KaeTheDev <kaethedev@gmail.com>
2026-01-29 00:41:07 -0500

    Created utils file recipes.ts and added helper function and Create function for localStorage

---
+++ src/utils/recipes.ts
@@ -0,0 +1,21 @@
+import type { Recipe } from "../types/Recipe";
+
+const STORAGE_KEY = "recipes";
+
+// Helper function to get all recipes 
+export function getRecipes(): Recipe[] {
+    // retrieves a string from localStorage
+    const data = localStorage.getItem(STORAGE_KEY);
+    // if data exists, convert JSON string into an array of Recipe objects
+    // otherwise, return an empty array
+    return data ? JSON.parse(data) : [];
+}
+
+// CREATE: Add a new recipe
+export function addRecipe(recipe: Recipe): void {
+    const recipes = getRecipes();
+    // Add the new recipe to the array
+    recipes.push(recipe);
+    // Saves updated array
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(recipes));
+}
